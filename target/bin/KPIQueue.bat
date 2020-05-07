bf0081258591c3f082674a80f38238b5fb987526
alexzhyshko <sanja.zh@gmail.com>
2020-05-07 12:55:47 +0300

    Major changes

--- target/bin/KPIQueue.bat
+++ target/bin/KPIQueue.bat
@@ -76,7 +76,7 @@ goto endInit
 @REM Reaching here means variables are defined and arguments have been captured
 :endInit
 
-%JAVACMD% %JAVA_OPTS% %EXTRA_JVM_ARGUMENTS% -classpath %CLASSPATH_PREFIX%;%CLASSPATH% -Dapp.name="KPIQueue" -Dapp.repo="%REPO%" -Dbasedir="%BASEDIR%" /KPIQueue/src/main/java/main/Main.java %CMD_LINE_ARGS%
+%JAVACMD% %JAVA_OPTS% %EXTRA_JVM_ARGUMENTS% -classpath %CLASSPATH_PREFIX%;%CLASSPATH% -Dapp.name="KPIQueue" -Dapp.repo="%REPO%" -Dbasedir="%BASEDIR%" main.Main %CMD_LINE_ARGS%
 if ERRORLEVEL 1 goto error
 goto end
 
